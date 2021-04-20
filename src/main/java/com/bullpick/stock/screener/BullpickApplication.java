fb5d56510c3c0f0dd522a3f841e91dcf0930b5d1
Joshua Mantooth <josh@Joshuas-MacBook-Pro-3.local>
2021-04-20 02:13:32 -0500

    initialize spring project

---
+++ src/main/java/com/bullpick/stock/screener/BullpickApplication.java
@@ -0,0 +1,13 @@
+package com.bullpick.stock.screener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullpickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullpickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bullpick/stock/screener/BullpickApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bullpick.stock.screener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullpickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
