21fa27c5ca067e21a7e1fcf7c30375c300adbe89
Joshua Mantooth <josh@Joshuas-MacBook-Pro-3.local>
2021-04-28 02:08:00 -0500

    Registration form populates users database and directs to registration success page

--- src/main/java/com/bullpick/stock/screener/controllers/HomeController.java
+++ src/main/java/com/bullpick/stock/screener/controllers/HomeController.java
@@ -1,12 +1,20 @@
 package com.bullpick.stock.screener.controllers;
 
 import com.bullpick.stock.screener.User;
+import com.bullpick.stock.screener.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private UserRepository repo;
+
+    @GetMapping("")
     public String viewHomePage() {
         return "index";
     }
@@ -15,5 +23,12 @@ public class HomeController {
     public String displayRegistrationForm(Model model) {
         model.addAttribute("user", new User());
         return "registration_form";
+    }
+
+    @PostMapping("/process_registration")
+    public String processUserRegistration(User user) {
+        repo.save(user);
+
+        return "registration_success";
     }
 }
