fca6298a2df36c9611ec7701365209683e9c53aa
Joshua Mantooth <josh@Joshuas-MacBook-Pro-3.local>
2021-04-28 01:38:51 -0500

    User Registration form template created for all fields. Not yet operational.

--- src/main/java/com/bullpick/stock/screener/controllers/HomeController.java
+++ src/main/java/com/bullpick/stock/screener/controllers/HomeController.java
@@ -1,10 +1,19 @@
 package com.bullpick.stock.screener.controllers;
 
+import com.bullpick.stock.screener.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
     public String viewHomePage() {
         return "index";
+    }
+
+    @GetMapping("/register")
+    public String displayRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "registration_form";
     }
 }
